b47a0e5c692376925513eeceb03546947fdc62a9
kienpros213 <kienpros213@gmail.com>
2022-10-16 15:21:54 +0700

    center a div!!!!!

--- travel-app/src/components/SearchBy.js
+++ travel-app/src/components/SearchBy.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {HStack} from '@chakra-ui/react';
+import SearchByDetail from './SearchByDetail';
 
 function SearchBy(){
     return(
-        <Box>
-            
-        </Box>
+        <HStack spacing={10} bg='tomato' display='flex' justifyContent='center'>
+            lmao
+            <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
+            <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
+            <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
+        </HStack>
     );
 }
 
