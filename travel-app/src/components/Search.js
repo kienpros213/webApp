70666e6e7b379f7bf93b0c29548d69e1c67f6e5f
kienpros213 <kienpros213@gmail.com>
2022-10-16 23:58:50 +0700

    img behind

--- travel-app/src/components/Search.js
+++ travel-app/src/components/Search.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {VStack, Input, Heading} from '@chakra-ui/react';
+import {VStack, Input, Heading, Image, Box} from '@chakra-ui/react';
 
 function Search(){
     return(
-        <VStack className="searchContainer" h='400' bg='#A5F1E9' display='flex' justifyContent='center'>
-            <Heading> Cùng bạn khám phá </Heading>
-            <Input placeholder='Search' w={400} bg='fffff' />
+        <VStack pos='relative' maxH={400} className="searchContainer" h='400' bg='#A5F1E9' display='flex' justifyContent='center' overflow='hidden'>
+            <Heading mb={100} pos='absolute' zIndex='6' > Cùng bạn khám phá </Heading>
+            <Input pos='absolute' zIndex='5' placeholder='Search' w={400} bg='fffff' />
+            <img className='bgImg' src='https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80' ></img>
         </VStack>
     );
 }
