2e8b64a1b537af2dd464628956fdbd3fd8f5befe
kienpros213 <kienpros213@gmail.com>
2022-10-19 21:43:49 +0700

    laptop

--- travel-app/src/components/Search.js
+++ travel-app/src/components/Search.js
@@ -4,7 +4,7 @@ import {VStack, Input, Heading, Image, Box} from '@chakra-ui/react';
 function Search(){
     return(
         <VStack pos='relative' maxH={400} className="searchContainer" h='400' bg='#A5F1E9' display='flex' justifyContent='center' overflow='hidden'>
-            <Heading mb={100} pos='absolute' zIndex='6' > Cùng bạn khám phá </Heading>
+            <Heading className='heading' mb={100} pos='absolute' zIndex='6' > Cùng bạn khám phá </Heading>
             <Input pos='absolute' zIndex='5' placeholder='Search' w={400} bg='fffff' />
             <img className='bgImg' src='https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80' ></img>
         </VStack>
--- travel-app/src/components/SearchBy.js
+++ travel-app/src/components/SearchBy.js
@@ -7,11 +7,11 @@ function SearchBy(){
         <Box bg='#E1FFEE' >
             <HStack pt={20} spacing={10} display='flex' justifyContent='center'>
                 <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
-                <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
-                <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
+                <SearchByDetail place = 'Thành phố Hà Nôi' number='71 địa điểm' />
+                <SearchByDetail place = 'Thành phố Thái Nguyên' number='31 địa điểm' />
             </HStack>
             <HStack pt={20} spacing={10} display='flex' justifyContent='center'>
-                <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
+                <SearchByDetail place = 'Thành phố Đà Nẵng' number='31 địa điểm' />
                 <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
                 <SearchByDetail place = 'Thành phố Hồ Chí Minh' number='31 địa điểm' />
             </HStack>
